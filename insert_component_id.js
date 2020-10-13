743fd0978f931914497db374d8676058d75f04b1
Farid Sabitov <farid.sabitov.design@gmail.com>
2020-10-13 14:09:53 +0300

    Updates

--- insert_component_id.js
+++ insert_component_id.js
@@ -1,43 +1,37 @@
-  
-
-letselectedFrames=figma.currentPage.selection
-letmasterComponents=[]
+let selectedFrames = figma.currentPage.selection
+let masterComponents = []
  
 // Select all master components
 selectedFrames.forEach(selectedFrame=>{
+  // For selected Frame
+  if(selectedFrame.type=="FRAME"){
+    let frameComponents = selectedFrame.findAll(n=>n.type==="COMPONENT")
+    if(frameComponents.length>0){
+      masterComponents=masterComponents.concat(frameComponents)
+    }
+  }
  
-// For selected Frame
-if(selectedFrame.type=="FRAME"){
-letframeComponents=selectedFrame.findAll(n=>n.type==="COMPONENT")
-if(frameComponents.length>0){
-masterComponents=masterComponents.concat(frameComponents)
-}
-}
- 
-// For selected Component
-if(selectedFrame.type=="COMPONENT"){
-masterComponents=masterComponents.concat(selectedFrame)
-}
+  // For selected Component
+  if(selectedFrame.type=="COMPONENT"){
+    masterComponents = masterComponents.concat(selectedFrame)
+  }
 })
- 
+  
 // Working with component description
-masterComponents.forEach(component=>{
-letcomponentDescription=component.description
+masterComponents.forEach(component => {
+  let componentDescription = component.description
  
-// Add Component ID
-addComponentID(component)
+  // Add Component ID
+  addComponentID(component)
+​
+  figma.notify("Component Description is updated")
 })
  
-functionaddComponentID(component){
-letresult=null
-
-// Check if we don't have component ID already 
-if(component.description.search("Design: ")==-1){
-print("Yes")
-
-// Add Component description
-component.description=component.description.concat("Design: "+component.id+"\n")
+function addComponentID(component){
+  // Check if we don't have component ID already 
+  if(component.description.search("Design: ")==-1){
+    // Add Component description
+    component.description=component.description.concat("\nDesign: "+component.id)
+  }
 }
-returnresult
-
-
+​
